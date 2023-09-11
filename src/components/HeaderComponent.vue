15ae8885d683f9fa41536918d4613c4bfa745ca9
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-09-10 20:26:59 -0700

    Main: Header and Navigation Menu Component created

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <section>
+            <NavComponent />
+            <article>
+                <h1>
+                    Hello, Welcome to Mountains
+                </h1>
+                <p>
+                    Mountains is a responsive website. You can use this mobile layout from
+                    <a href="#"><span>Website</span>Mo</a>. Please, tell your friends about our website.
+                </p>
+                <div>
+                    <a href="#">login</a>
+                    <a href="#">sign up</a>
+                </div>
+            </article>
+        </section>
+    </header>
+</template>
+
+<script>
+import NavComponent from './NavComponent.vue';
+
+
+export default {
+    name: "HeaderComponent",
+    components: {
+        NavComponent
+    }
+}
+</script>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <MegaMenu />
+        <MobileMenu />
+    </nav>
+</template>
+
+<script>
+import MegaMenu from './menus/MegaMenu.vue';
+import MobileMenu from './menus/MobileMenu.vue';
+
+export default {
+    name: "NavComponent",
+    components: {
+        MegaMenu,
+        MobileMenu
+    }
+}
+</script>
