c487ca034437f5831f5f7defb9de19a5cdbdedf3
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-09-10 21:12:20 -0700

    Main: Main Content Component done

---
+++ src/components/MainContent.vue
@@ -0,0 +1,37 @@
+<template>
+    <main>
+        <!-- ABOUT SECTION -->
+        <AboutComponent />
+        <!-- ABOUT SECTION END -->
+        <!-- FEATURES SECTION -->
+        <FeaturesComponent />
+        <!-- FEATURES SECTION END -->
+        <!-- SUBSCRIBE SECTION -->
+        <SubscribeComponent />
+        <!-- SUBSCRIBE SECTION END -->
+        <!-- CONTACT SECTION -->
+        <ContactComponent />
+        <!-- CONTACT SECTION END -->
+        <a href="#">
+            GO TO TOP
+        </a>
+    </main>
+</template>
+
+<script>
+import AboutComponent from './content/AboutComponent.vue';
+import FeaturesComponent from './content/FeaturesComponent.vue';
+import SubscribeComponent from './content/SubscribeComponent.vue';
+import ContactComponent from './content/ContactComponent.vue';
+
+export default {
+    name: "MainContent",
+    components: {
+        AboutComponent,
+        FeaturesComponent,
+        SubscribeComponent,
+        ContactComponent
+    }
+}
+
+</script>
